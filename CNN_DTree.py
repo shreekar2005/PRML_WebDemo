d83a73bb631c7877bedb7188f315cff326c6748a
CCLAB <cclab1@lab.iitj.ac.in>
2025-04-08 01:22:03 +0530

    adding backedn files

--- CNN_DTree.py
+++ CNN_DTree.py
@@ -1,26 +1,21 @@
-import DTree
+import pickle
 import CNN_preprocessing
+import DTree
 
+# Load the saved model from the file
+with open("tree_CNN_model.pkl", "rb") as f:
+    tree_CNN_loaded = pickle.load(f)
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+name_image_dict = {
+    
 
+} # name : path of his base image
 
+def predict(image_path):
+    features= CNN_preprocessing.extract_features(image_path)
+    predicted_name = DTree.predict(tree_CNN_loaded,[features])
+    predicted_image_path= None
+    return int(predicted_name[0]), predicted_image_path
 
-url_filtered_CNN_features_dataset = 'https://raw.githubusercontent.com/AgarwalMayank2/Face_Detection/refs/heads/main/processed_dataset/filtered_CNN_features_dataset.csv' #for CNN limited
-df_CNN = pd.read_csv(url_filtered_CNN_features_dataset) # reading url for extracted CNN_features_dataset_limited.csv
-df_CNN.drop('Unnamed: 0', axis=1, inplace=True)
-
-#dropping those labels whose number of datapoints are less than 80
-# Get the counts of each label
-label_counts = df_CNN['2048'].value_counts()
-
-# Filter out labels with counts less than 80
-labels_to_keep = label_counts[label_counts >= 80].index
-
-# Filter the DataFrame
-df_CNN = df_CNN[df_CNN['2048'].isin(labels_to_keep)]
-
-df_CNN
-
+if __name__ == "__main__":
+    print(predict("testimage.jpg"))
